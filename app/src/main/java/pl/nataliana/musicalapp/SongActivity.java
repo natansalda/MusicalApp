8b210262dad9c4de7e8949d60541c3acb5465998
Natalka <nazarjazz@wp.pl>
2017-04-30 23:32:47 +0200

    Buttons added to activities

--- app/src/main/java/pl/nataliana/musicalapp/SongActivity.java
+++ app/src/main/java/pl/nataliana/musicalapp/SongActivity.java
@@ -1,7 +1,10 @@
 package pl.nataliana.musicalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static pl.nataliana.musicalapp.R.id.text_field;
@@ -24,5 +27,51 @@ public class SongActivity extends AppCompatActivity {
         //Set content to text
         TextView text_fieldText = (TextView) findViewById(text_field);
         text_fieldText.setText(R.string.text_song);
+
+        // Find the View that shows the particular button
+        Button playlist = (Button) findViewById(R.id.playlist);
+        Button song = (Button) findViewById(R.id.song);
+        Button random = (Button) findViewById(R.id.trending);
+        Button premium = (Button) findViewById(R.id.premium);
+
+        // Set a click listener on that View
+        playlist.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the playlist View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playlistIntent = new Intent(SongActivity.this, PlaylistActivity.class);
+                startActivity(playlistIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        song.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the song View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent songIntent = new Intent(SongActivity.this, SongActivity.class);
+                startActivity(songIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        random.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the trending View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent trendingIntent = new Intent(SongActivity.this, TrendingActivity.class);
+                startActivity(trendingIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        premium.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the premium View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent premiumIntent = new Intent(SongActivity.this, PremiumActivity.class);
+                startActivity(premiumIntent);
+            }
+        });
     }
 }
